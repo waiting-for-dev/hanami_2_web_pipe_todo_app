7ade29b859f980947ebf952835775193cb9001b6
Marc Busqué <marc@lamarciana.com>
2021-09-21 13:50:23 +0200

    Change description to be text instead of varchar in the db

--- db/migrate/20210913100727_create_todos.rb
+++ db/migrate/20210913100727_create_todos.rb
@@ -5,7 +5,7 @@ ROM::SQL.migration do
     create_table(:todos) do
       primary_key :id
       column :title, String, null: false
-      column :description, String, null: false
+      column :description, String, text: true, null: false
     end
   end
 end
